d5b71701adacd27898976e9c6f04b15cb17dddbf
Brandon McAnsh <brandonm@matricom.net>
2014-04-10 15:54:31 -0400

    Initial 4.4.2 kernel source
    Signed-off-by: Brandon McAnsh <brandonm@matricom.net>

---
+++ include/linux/amlogic/vmapi.h
@@ -0,0 +1,20 @@
+#ifndef VM_API_INCLUDE_
+#define VM_API_INCLUDE_
+
+typedef struct vm_output_para{
+	int width;
+	int height;
+	int bytesperline;
+	int v4l2_format;
+	int index;
+	int v4l2_memory;
+	int zoom;     // set -1 as invalid
+	int mirror;   // set -1 as invalid
+	int angle;
+	unsigned vaddr;
+	unsigned int ext_canvas;
+}vm_output_para_t;
+
+int vm_fill_buffer(struct videobuf_buffer* vb , vm_output_para_t* para);
+
+#endif /* VM_API_INCLUDE_ */
