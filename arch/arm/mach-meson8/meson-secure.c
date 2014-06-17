98e8ac3db99555f3fb9b9527f7e10b0176c12733
KhasMek <Boushh@gmail.com>
2014-06-17 08:29:17 -0600

    update meson8 architecture
      pulled from arm-src-kernel-2014-05-26-c53de253cb
    
    Signed-off-by: KhasMek <Boushh@gmail.com>

--- arch/arm/mach-meson8/meson-secure.c
+++ arch/arm/mach-meson8/meson-secure.c
@@ -35,7 +35,7 @@
 #endif
 #include <linux/sched.h>
 
-#define MESON_SECURE_DEBUG 1
+#define MESON_SECURE_DEBUG 0
 #ifdef MESON_SECURE_DEBUG
 #define TZDBG(fmt, args...) printk("meson-secure: " fmt, ## args);
 #else
@@ -67,10 +67,11 @@ int meson_secure_memblock(unsigned startaddr, unsigned endaddr, struct secure_me
 */
 
 struct memconfig memsecure[MEMCONFIG_NUM] = {0};
-int meson_trustzone_memconfig()
+int meson_trustzone_memconfig(void)
 {
 	int ret;
 	struct memconfig_hal_api_arg arg;
+
 	arg.memconfigbuf_phy_addr = __pa(memsecure);
 	arg.memconfigbuf_count = MEMCONFIG_NUM;
 
@@ -89,7 +90,7 @@ int meson_trustzone_memconfig()
 	return ret;
 }
 
-unsigned int meson_trustzone_getmemsecure_size()
+unsigned int meson_trustzone_getmemsecure_size(void)
 {
 	unsigned int size = 0;
 	int i;
@@ -145,6 +146,7 @@ int meson_trustzone_efuse(struct efuse_hal_api_arg* arg)
 	}
 	outer_inv_range((arg->retcnt_phy), (arg->retcnt_phy + sizeof(unsigned int)));
 	dmac_unmap_area(__va(arg->buffer_phy), arg->size, DMA_FROM_DEVICE);
+	set_cpus_allowed_ptr(current, cpu_all_mask);
 
 	return ret;
 }
@@ -162,6 +164,7 @@ uint32_t meson_secure_reg_read(uint32_t addr)
 
 	return ret;
 }
+EXPORT_SYMBOL(meson_secure_reg_read);
 
 uint32_t meson_secure_reg_write(uint32_t addr, uint32_t val)
 {
@@ -176,13 +179,14 @@ uint32_t meson_secure_reg_write(uint32_t addr, uint32_t val)
 
 	return ret;
 }
+EXPORT_SYMBOL(meson_secure_reg_write);
 
-uint32_t meson_secure_mem_size(vold)
+uint32_t meson_secure_mem_size(void)
 {
 	return MESON_TRUSTZONE_MEM_SIZE;
 }
 
-uint32_t meson_secure_mem_end(vold)
+uint32_t meson_secure_mem_end(void)
 {
 	return (MESON_TRUSTZONE_MEM_START + MESON_TRUSTZONE_MEM_SIZE);
 }
